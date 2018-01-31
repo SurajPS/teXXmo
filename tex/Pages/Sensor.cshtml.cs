823a9475fe8183f17231b7378ba8b1fd5029f5a1
SurajPS <mukundsuraj@gmail.com>
2018-01-31 13:08:57 +0100

    Projektdateien hinzufügen.

---
+++ tex/Pages/Sensor.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tex.Pages
+{
+    public class SensorModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your Sensor application description page.";
+        }
+    }
+}
